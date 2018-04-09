d02130902def45d359b52ee02f94d4bc42737199
Nicol Vojacek <nvojacek@gmail.com>
2018-04-09 20:28:10 +0200

    Add post

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+def get_sender_name(form_data):
+    return form_data['sender_name']
+
+
+def get_response_message(form_data):
+    sender_name = get_sender_name(form_data)
+    friends = get_friend_list(form_data)
+    return 'Hi {}! I have invited {}.'.format(sender_name, friends)
+
+
+def get_friend_list(form_data):
+    return form_data['text']
+
+
+@app.route("/caffeinetrip", methods=["post"])
+def caffeinetrip():
+    form_data = request.form
+    response_message = get_response_message(form_data)
+    return response_message, 200
