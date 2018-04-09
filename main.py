f3d4ce6e610d60a176d73d2f89151d946cf522c5
Nicol Vojacek <nvojacek@gmail.com>
2018-04-09 21:43:38 +0200

    Test send message

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
+import os
 from flask import Flask, request
+from slackclient import SlackClient
 app = Flask(__name__)
 
 
-def get_sender_name(form_data):
-    return form_data['sender_name']
+SLACK_TOKEN = os.environ.get('SLACK_TOKEN')
+slack_client = SlackClient(SLACK_TOKEN)
+
+
+def send_message(user_id, message):
+    slack_client.api_call(
+        'chat.postMessage',
+        user=user_id,
+        text=message,
+        username='charlie',
+        icon_emoji=':coffee:'
+    )
+
+
+def get_user_name(form_data):
+    return form_data['user_name']
 
 
 def get_response_message(form_data):
-    sender_name = get_sender_name(form_data)
+    sender_name = get_user_name(form_data)
     friends = get_friend_list(form_data)
     return 'Hi {}! I have invited {}.'.format(sender_name, friends)
 
@@ -20,4 +36,15 @@ def get_friend_list(form_data):
 def caffeinetrip():
     form_data = request.form
     response_message = get_response_message(form_data)
+    user_id = form_data['user_id']
+    user_message = 'Do you want coffee?'
+    send_message(user_id, user_message)
     return response_message, 200
+
+
+def main():
+    app.run()
+
+
+if __name__ == '__main__':
+    main()
